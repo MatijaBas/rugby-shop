d0c1d72b3da16d44b25e4ed91012a2c2b3a19a73
Matija Basanovic <mbasanov@gmail.com>
2020-03-09 10:56:23 +0000

    Finishing login and logout authentication

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages, auth
+from django.contrib.auth.decorators import login_required
 from accounts.forms import UserLoginForm
 
 
@@ -8,6 +9,7 @@ def index(request):
     return render(request,  'index.html')
 
 
+@login_required
 def logout(request):
     """A view to log the user out and redirect back to the index page"""
     auth.logout(request)
@@ -17,16 +19,19 @@ def logout(request):
 
 def login(request):
     """A view to manage the login form"""
+    if request.user.is_authenticated:
+        return redirect(reverse('index'))
     if request.method == "POST":
         login_form = UserLoginForm(request.POST)
 
         if login_form.is_valid():
             user = auth.authenticate(username=request.POST['username'],
                                      password=request.POST['password'])
+            messages.success(request, "You have successfully logged in!")
 
             if user:
                 auth.login(user=user, request=request)
-                messages.success(request, "You have successfully logged in!")
+                return redirect(reverse('index'))
             else:
                 login_form.add_error(
                     None, "Your username or password is incorrect")
