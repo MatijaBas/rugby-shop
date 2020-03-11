6a0876499cb7d2e412704d4a64cdd806a58a59e3
Matija Basanovic <mbasanov@gmail.com>
2020-03-11 10:35:03 +0000

    added password_reset templates

--- rugbyshop/settings.py
+++ rugbyshop/settings.py
@@ -121,3 +121,10 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_PORT = 587
+# EMAIL_USE_TLS = True
+# EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
+# EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
