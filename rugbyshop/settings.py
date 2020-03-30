e247e78b3b45a66c1bf248a5814124a97516d199
Matija Basanovic <mbasanov@gmail.com>
2020-03-30 21:13:27 +0000

    editing profile.html

--- rugbyshop/settings.py
+++ rugbyshop/settings.py
@@ -177,10 +177,9 @@ STRIPE_SECRET = os.getenv('STRIPE_SECRET')
 MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
 
 # Config to send emails
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_USE_TLS = True
 EMAIL_HOST = "smtp.gmail.com"
-FROM_EMAIL = 'mattbassdeveloper@gmail.com'
 EMAIL_HOST_USER = os.environ.get('EMAIL_ADDRESS')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD')
 EMAIL_PORT = 587
