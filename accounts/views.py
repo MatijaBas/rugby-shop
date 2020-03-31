abb01e0670b8f9448a4387eb6a91cb996e5573d6
Matija Basanovic <mbasanov@gmail.com>
2020-03-31 13:56:05 +0000

    change views.py

--- accounts/views.py
+++ accounts/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect, reverse
 from django.contrib import messages, auth
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
 from accounts.forms import UserLoginForm, UserRegistrationForm
 
 
