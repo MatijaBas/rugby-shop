5f364bd75a5f83fc2039b6ccab3206b34a6ce286
Matija Basanovic <mbasanov@gmail.com>
2020-04-02 19:50:23 +0000

    working on password-reset

--- rugbyshop/settings.py
+++ rugbyshop/settings.py
@@ -180,7 +180,6 @@ MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_USE_TLS = True
 EMAIL_HOST = "smtp.gmail.com"
-FROM_EMAIL = "mattbassdeveloper@gmail.com"
 EMAIL_HOST_USER = os.environ.get('EMAIL_ADDRESS')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD')
 EMAIL_PORT = 587
