5473c3a3d89743bd1bb166d2a1258d0ea4801612
Matija Basanovic <mbasanov@gmail.com>
2020-04-16 19:45:08 +0000

    added some tests to reviews

--- reviews/tests.py
+++ reviews/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from .models import Review
 
 # Create your tests here.
+
+
+class ReviewTests(TestCase):
+
+    def test_str(self):
+        test_title = Review(title='Good jersey')
+        self.assertEqual(str(test_title), 'Good jersey')
